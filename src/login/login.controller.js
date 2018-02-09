070ecfec5b3bf57f3beee3f243f4069a918dc31b
Pragya5h9 <pragyajha@aec.com>
2018-02-09 15:49:52 +0530

    login

---
+++ src/login/login.controller.js
@@ -0,0 +1,12 @@
+class LoginController{
+    constructor()
+    {
+    this.email="";
+    this.password="";
+    }
+    doLogin(){
+       console.log(this.email);
+       console.log(this.password);
+    }
+}
+export default LoginController;
